4f5b05ff0fb42db0a4cafdaf8a07edfb81980e8a
Christian Henkel <post@henkelchristian.de>
2025-01-30 21:39:41 +0100

    a lit parser
    Signed-off-by: Christian Henkel <post@henkelchristian.de>

--- bibtex/_get_lit.py
+++ bibtex/_get_lit.py
@@ -23,8 +23,17 @@ import os
 from typing import List, Set
 import bibtexparser
 import re
+import argparse
+import sys
+import difflib
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check", action="store_true")
+    args = parser.parse_args()
+    do_check = args.check
+    print(f"{do_check=}")
+
     expected_pwd: str = "hcde/bibtex"
     current_pwd = os.getcwd()
     assert (
@@ -42,8 +51,10 @@ if __name__ == "__main__":
 
     cited_keys: Set[str] = set()
 
-    dir_blog_posts = os.path.join(current_pwd, '..', 'docs/posts/ton')
-    assert os.path.exists(dir_blog_posts), f"Blog posts directory must exist. {dir_blog_posts}"
+    dir_blog_posts = os.path.join(current_pwd, "..", "docs/posts/ton")
+    assert os.path.exists(
+        dir_blog_posts
+    ), f"Blog posts directory must exist. {dir_blog_posts}"
 
     for filename in os.listdir(dir_blog_posts):
         if filename.endswith(".md"):
@@ -52,12 +63,54 @@ if __name__ == "__main__":
                 content = file.read()
                 # find cited keys in the form of
                 # [@key1] or [@key2,@key3]
-                matches = re.findall(r'\[@(.*?)\]', content)
+                matches = re.findall(r"\[@(.*?)\]", content)
                 for match in matches:
-                    keys = match.split(',')
+                    keys = match.split(",")
                     for key in keys:
-                        cited_keys.add(key.replace('@', '').strip())
+                        cited_keys.add(key.replace("@", "").strip())
 
-    print(f"{len(cited_keys)} unique keys found.")
-    print("e.g:", ', '.join(list(cited_keys)[:3]))
+    sorted_cited_keys = sorted(cited_keys)
+    print(f"{len(sorted_cited_keys)} unique keys found.")
+    print("e.g:", ", ".join(list(sorted_cited_keys)[:3]))
 
+    target_lib = bibtexparser.bibdatabase.BibDatabase()
+    for key in sorted_cited_keys:
+        try:
+            entry = library.entries_dict[key]
+            if "howpublished" in entry:
+                entry["url"] = entry["howpublished"]
+                entry.pop("howpublished", None)
+        except KeyError as e:
+            print(f"KeyError: {e}")
+            sys.exit(os.EX_DATAERR)
+        target_lib.entries.append(entry)
+
+    if do_check:
+        if not os.path.exists(target_bibtex):
+            print(f"{target_bibtex} does not exist.")
+            exit(1)
+        
+        with open(target_bibtex, "r", encoding="utf-8") as f:
+            existing_lib = bibtexparser.load(f)
+        
+        existing_entries_str = bibtexparser.dumps(existing_lib)
+        target_entries_str = bibtexparser.dumps(target_lib)
+        diff = difflib.unified_diff(
+            existing_entries_str.splitlines(),
+            target_entries_str.splitlines(),
+            fromfile="existing",
+            tofile="target",
+            lineterm="",
+        )
+        diff_list = list(diff)
+        if not diff_list:
+            print("The files match.")
+            sys.exit(os.EX_OK)
+        for line in diff_list:
+            print(line)
+        print("The files do not match.")
+        sys.exit(os.EX_DATAERR)
+    else:
+        with open(target_bibtex, "w", encoding="utf-8") as f:
+            bibtexparser.dump(target_lib, f)
+        print(f"Written {len(target_lib.entries)} entries to {target_bibtex}.")
